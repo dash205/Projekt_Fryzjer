e9407676be43d216d6164a8c80668f23ae112360
dash205 <dashmati20@gmail.com>
2026-05-29 14:51:33 +0200

    test qsltablemodel

--- clients.h
+++ clients.h
@@ -1,19 +1,13 @@
-//
-// Created by Artur on 19.05.2026.
-//
-
 #ifndef PROJEKT_FRYZJER_CLIENTS_H
 #define PROJEKT_FRYZJER_CLIENTS_H
 
 #include <QWidget>
-
+#include <QSqlTableModel>
 
 QT_BEGIN_NAMESPACE
-
 namespace Ui {
     class Clients;
 }
-
 QT_END_NAMESPACE
 
 class Clients : public QWidget {
@@ -21,12 +15,16 @@ class Clients : public QWidget {
 
 public:
     explicit Clients(QWidget *parent = nullptr);
-
     ~Clients() override;
+
+private slots:
+    void on_btnAdd_clicked();
+    void on_btnDelete_clicked();
+    void on_btnSave_clicked();
 
 private:
     Ui::Clients *ui;
+    QSqlTableModel *clientModel;
 };
-
 
 #endif //PROJEKT_FRYZJER_CLIENTS_H
